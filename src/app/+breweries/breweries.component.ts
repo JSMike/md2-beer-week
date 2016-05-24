63678c6105ceb3298a31907770d6c3ae2889d815
Michael <mcebri01@villanova.edu>
2016-05-24 16:47:02 -0400

    Add cards for breweries

--- src/app/+breweries/breweries.component.ts
+++ src/app/+breweries/breweries.component.ts
@@ -1,16 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { OnActivate, Router, RouteSegment } from '@angular/router';
+import { Brewery } from '../shared/brewery.model';
+import { BreweryService } from '../shared/brewery.service';
+import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
+import { MdButton } from '@angular2-material/button';
 
 @Component({
   moduleId: module.id,
   selector: 'app-breweries',
   templateUrl: 'breweries.component.html',
-  styleUrls: ['breweries.component.css']
+  styleUrls: ['breweries.component.css'],
+  directives: [
+    MD_CARD_DIRECTIVES,
+    MdButton
+  ]
 })
-export class BreweriesComponent implements OnInit {
+export class BreweriesComponent implements OnActivate {
+  breweries: Brewery[];
+  router: Router;
+  breweryService: BreweryService;
 
-  constructor() {}
-
-  ngOnInit() {
+  constructor(breweryService: BreweryService, router: Router) {
+    this.breweryService = breweryService;
+    this.router = router;
   }
+
+  routerOnActivate(route: RouteSegment): void {
+    let id: number = +route.getParam('id');
+    this.breweryService.getBreweries()
+      .then(breweries => this.breweries = breweries);
+  }
+
+  viewBrewer(id: number): void {
+    this.router.navigate(['/brewery', id]);
+  }
+
 
 }
