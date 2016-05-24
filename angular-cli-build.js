e86ae278479239a7b46e6a8a56bf295eb2756905
Michael <mcebri01@villanova.edu>
2016-05-24 10:30:14 -0400

    Add Angular2-Material

--- angular-cli-build.js
+++ angular-cli-build.js
@@ -11,7 +11,9 @@ module.exports = function(defaults) {
       'es6-shim/es6-shim.js',
       'reflect-metadata/**/*.+(js|js.map)',
       'rxjs/**/*.+(js|js.map)',
-      '@angular/**/*.+(js|js.map)'
+      '@angular/**/*.+(js|js.map)',
+      '@angular2-material/**/*.+(js|js.map)',
+      'lodash/lodash.js'
     ]
   });
 };
